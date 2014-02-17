e3816378611437102fe20ddd4821aa4f160d9eca
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2014-02-17 21:52:43 +0100

    pretty-logs.py: Read full json

--- pretty-logs.py
+++ pretty-logs.py
@@ -10,46 +10,34 @@ view: ./pretty-logs.py < mylogs.log
 import sys
 import json
 
-def find_end(line, start):
-    x = 0
-    for i,c in enumerate(line[start:]):
-        if c in ('{','['):
-            x += 1
-        elif c in ('}',']'):
-            x -= 1
-        if x == 0:
-            return start+i+1
-    return len(line)
-
-def find_start(line, start):
-    for i,c in enumerate(line[start:]):
-        if c == '{':
-            return start+i
-    return len(line)
 
 fd = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
 
 from pprint import pprint
 import traceback
 
+def format_traceback_part(i, tb_part):
+    s = '{%s} ' % i
+    if 'file' in tb_part:
+        s += tb_part['file']
+        if 'line' in tb_part:
+            s += '(%s)' % tb_part['line']
+        s += ': '
+    if 'class' in tb_part:
+        s += tb_part['class']+tb_part['type']
+    s += '%s(%s)' % (
+        tb_part['function'],
+        ', '.join(( str(x) for x in tb_part['args'] ))
+    )
+    return s
+
 def clean(d):
     if isinstance(d, dict):
         for k,v in d.items():
-            if k == 'trace':
+            if k == 'trace' and len(d[k]) > 0 and isinstance(d[k][0], dict):
                 trace = []
                 for i,l in enumerate(d[k]):
-                    s = '{%s} ' % i
-                    if 'file' in l:
-                        s += l['file']
-                        if 'line' in l:
-                            s += '(%s)' % l['line']
-                        s += ': '
-                    if 'class' in l:
-                        s += l['class']+l['type']
-                    s += '%s(%s)' % (
-                        l['function'],
-                        ', '.join(( str(x) for x in l['args'] ))
-                    )
+                    s = format_traceback_part(i, l)
                     trace.append(s)
                 d['trace'] = trace
             else:
@@ -58,27 +46,16 @@ def clean(d):
 
 while (1):
     line = fd.readline()
-    if not line: break
-    start = find_start(line, 0)
-    print line[:start]
-    end = find_end(line, start)
+    if not line: continue
     try:
-        d = json.loads(line[start:end])
+        d = json.loads(line)
         d = clean(d)
         print json.dumps(d, indent=4)
+        #pprint(d)
     except ValueError:
         traceback.print_exc()
-        if line[start:end].strip(): print line[start:end]
-    start = find_start(line, end)
-    end = find_end(line, start)
-    try:
-        d = json.loads(line[start:end])
-        d = clean(d)
-        print json.dumps(d, indent=4)
-    except ValueError:
-        traceback.print_exc()
-        if line[start:end].strip(): print line[start:end]
+        print line
     print
-    print
+    
         
 
